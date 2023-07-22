2de156ebbf45ef66bd96b774b2a15410a212a146
h0lybyte <5599058+h0lybyte@users.noreply.github.com>
2023-07-22 14:15:27 -0400

    Sync + Minor Updates
    Issue: https://github.com/KBVE/kbve.com/issues/702

--- rentearth/packages/ui/src/LoginForm.tsx
+++ rentearth/packages/ui/src/LoginForm.tsx
@@ -1,37 +1,24 @@
-import { Button, Form, H3, H4, SizableText, Spinner, XStack, YStack } from 'tamagui'
-
-import React, { useEffect, useState } from 'react'
-
-import { email$, password$, flow$, status$, tasker, VE } from './library/Storage'
+import { Button, H3, XStack, YStack } from 'tamagui'
+import { email$, password$, error$, status$ } from './library/Storage'
 import { FormField, FormLink, FormForm } from './library/Form'
+import { minionLogin } from './library/appwrite'
 import { useStore } from '@nanostores/react'
 
 export const LoginForm = () => {
-  const $status = useStore(status$)
-
-  useEffect(() => {
-    if ($status === 'submitting') {
-      const timer = setTimeout(() => tasker(status$, undefined), 4000)
-      return () => {
-        clearTimeout(timer)
-      }
-    }
-  }, [$status])
+  if (useStore(status$) === 'submitting') minionLogin(email$, password$, status$, error$)
   return (
     <YStack jc="center" ai="center" p="$4" space>
       <Button>
         <H3>Hi! Welcome</H3>
       </Button>
       <XStack space>
-        <FormForm data={'A more productive you is waiting inside this app. Please enter your details!'}>
-
+        <FormForm
+          data={'A more productive you is waiting inside this app. Please enter your details!'}
+        >
           <FormField formId={'login'} data={'Email'} storage={email$} />
           <FormField formId={'login'} data={'Password'} storage={password$} secure={true} />
-        
         </FormForm>
-          
       </XStack>
-
       <FormLink data={'Forgot Password?'} link={'/account/recover'} />
       <FormLink data={"Don't have an account? Sign Up!"} link={'/account/register'} />
     </YStack>
